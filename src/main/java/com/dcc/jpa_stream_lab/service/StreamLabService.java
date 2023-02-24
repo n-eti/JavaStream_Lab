0558deaeba92c742d74c29c2c73b6a46e2b18419
Nada Atieh <nada.s.atieh@gmail.com>
2023-02-24 14:04:06 -0600

    Solved problems one through three

--- src/main/java/com/dcc/jpa_stream_lab/service/StreamLabService.java
+++ src/main/java/com/dcc/jpa_stream_lab/service/StreamLabService.java
@@ -46,8 +46,9 @@ public class StreamLabService {
         // Return the COUNT of all the users from the User table.
         // You MUST use a .stream(), don't listen to the squiggle here!
         // Remember yellow squiggles are warnings and can be ignored.
-    	return 0;
+    	return users.findAll().stream().count();
     }
+
 
     public List<Product> RDemoTwo()
     {
@@ -59,14 +60,14 @@ public class StreamLabService {
     {
         // Write a query that gets each product whose price is less than or equal to $100.
         // Return the list
-        return null;
+        return products.findAll().stream().filter(p -> p.getPrice() <100).toList();
     }
 
     public List<Product> RProblemThree()
     {
         // Write a query that gets each product that CONTAINS an "s" in the products name.
         // Return the list
-    	return null;
+    	return products.findAll().stream().filter(p -> p.getName().contains("s")).toList();
     }
 
     public List<User> RProblemFour()
@@ -76,7 +77,7 @@ public class StreamLabService {
         // Research 'java create specific date' and 'java compare dates'
         // You may need to use the helper classes imported above!
     	
-        return null;
+        return products.findAll().stream().filter(p -> p.)
     }
 
     public List<User> RProblemFive()
